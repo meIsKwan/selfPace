e746f33953084a5ab267728f87da9b522645fe2e
meIsKwan <mc.minhquan612@gmail.com>
2024-10-14 22:50:57 +0700

    my first code with C++

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-consteval int get_value(){
-    return 3;
-}
+int main(){ 
 
-int main(){
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    /*
+        This code one of my first code for C++
+        I will check it again if I have time and my career on this way
+    */
+
+    std::cout << "Hello bitch" << std::endl;
+    std::cout << "This world is mine" << std::endl;
+    std::cout << "We will learn C++" << std::endl;
+
     return 0;
 }
