d9a76290a6ae4dfd91f759207281c8582430051a
meIsKwan <mc.minhquan612@gmail.com>
2024-10-15 21:52:37 +0700

    test

--- main.cpp
+++ main.cpp
@@ -2,17 +2,16 @@
 #include <string>
 
 int main(){
-    std::string myName;  // Use std::string instead of char for the name
+    char myName;
     int age;
+    std::cout << "Write your name here" << std::endl;
+    std::cin >> myName;
 
-    std::cout << "Write your name here: " << std::endl;
-    std::getline(std::cin, myName);  // Use std::getline to read the full name
-
-    std::cout << "Write your age here: " << std::endl;
+    std::cout << "Write your age here" << std::endl;
     std::cin >> age;
 
     std::cout << "Your age is: " << age << std::endl;
-    std::cout << "Your name is: " << myName << std::endl;
+    std::cout << "Your name is: " << myName << std::endl; 
 
     return 0;
 }
