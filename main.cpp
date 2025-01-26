705b62c47bcc4185001da584012209b5358e3b39
meIsKwan <mc.minhquan612@gmail.com>
2025-01-26 09:10:29 +0700

    hehe

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <string>
+#include <locale>
+#include <io.h>
+#include <fcntl.h>
 
 int main(){
     int cuccut {10};
     long long dicho {123};
-    std::cout << cuccut << " và 0 " << dicho << std::endl;
+    std::cout << cuccut << " and 0 " << dicho << std::endl;
     std::cout << sizeof(dicho) << " is the size of 'dicho' " << dicho << std::endl;
 }
