1a3e9c1b130570c68f2fbefec84639fd3dd5f079
meIsKwan <mc.minhquan612@gmail.com>
2025-01-27 14:04:18 +0700

    hahaha

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include <locale>
-#include <io.h>
-#include <fcntl.h>
 
-int main(){
-    int cuccut {10};
-    long long dicho {123};
-    std::cout << cuccut << " and 0 " << dicho << std::endl;
-    std::cout << sizeof(dicho) << " is the size of 'dicho' " << dicho << std::endl;
+int main()
+{
+    int amount_dog(10);
+
+    int amount_cat{4};
+    // Use '(###)' for int '{###}' will return error
+    int fraction_int (17450.2);
+
+    int amount_animals {amount_cat + amount_dog};
+
+    std::cout << "Total animals is: " << amount_animals << std::endl;
+    std::cout << "Total animals is: " << fraction_int << std::endl;
+
+    std::cout << "side of int: " << sizeof(int) << std::endl;
+    std::cout << "side of fraction_int: " << sizeof(fraction_int) << std::endl;
+
+    return 0;
 }
